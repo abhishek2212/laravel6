715b3f710f83e870ac3a177e0b1872fd7bdfe424
abhishek2212 <abhishektiwarinetid@gmail.com>
2020-04-11 16:49:00 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pizzas', function () {
+    // get the data from database
+    $pizza= [
+        'fname' => 'Abhishek', 
+        'lname' => 'Tiwari',
+        'job' => 'Software Engineer'
+
+    ];
+    return view('pizzas', $pizza);
+});
